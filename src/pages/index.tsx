a684d1aaca98f9212b181e922ec45044c7acdcd3
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-11-17 23:52:18 -0300

    chore: add functions to cunsumer api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import { client } from "../graphql/client/graphql-client";
+import { QueryRandomJoke } from "../graphql/queries/getRandomJoke";
+import { QuerySearchJokes } from "../graphql/queries/searchJoke";
+
+const getRandomJoke = async () => {
+  return await client({ query: QueryRandomJoke });
+}
+
+const searchJoke = async (q: string) => {
+  return await client({ query: QuerySearchJokes, variables: { query: q } });
+}
+
 export default function Home() {
   return (
     <div >
