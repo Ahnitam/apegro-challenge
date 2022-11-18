cb1fda6bb94827723651643d33e8daefa0c75609
Higor Pires de França <higorpiresdefranca60@gmail.com>
2022-11-17 23:53:24 -0300

    chore: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
-import { client } from "../graphql/client/graphql-client";
+import Image from "next/image";
+import { useState } from "react";
+import Button from "../components/Button";
+import Head from "../components/Head";
+import Search from "../components/Search";
 import { QueryRandomJoke } from "../graphql/queries/getRandomJoke";
 import { QuerySearchJokes } from "../graphql/queries/searchJoke";
+import { client } from "../graphql/client/graphql-client";
+import { Joke } from "../types/joke";
+import JokesResul from "../components/JokesResult";
 
 const getRandomJoke = async () => {
   return await client({ query: QueryRandomJoke });
@@ -11,9 +18,41 @@ const searchJoke = async (q: string) => {
 }
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [jokes, setjokes] = useState<Joke[]>([]);
+
+  const _onSearch = async (q: string) => {
+    setIsLoading(true);
+    searchJoke(q || "").then((e) => {
+      setIsLoading(false);
+      setjokes(e.data.searchJokes || []);
+    });
+  };
+
+  const _onClickRandom = async () => {
+    setIsLoading(true);
+    getRandomJoke().then((e) => {
+      setjokes([e.data.getRandomJoke]);
+      setIsLoading(false);
+    });
+  }
+
   return (
-    <div >
-      Hello World!
+    <div className="h-screen flex flex-col items-center">
+      <Head />
+      <header className="p-4">
+        <Image priority={true} src="/chuck_norris.png" alt="Chuck Norris" width="150" height="150" />
+      </header>
+      <main className="flex-1">
+        <div className="h-full flex flex-col items-center">
+          <Search placeholder="Digite sua pesquisa" onSearch={_onSearch} />
+          <Button label="Fato Aleatório" onClick={_onClickRandom} />
+          <JokesResul isLoading={isLoading} jokes={jokes} />
+        </div>
+      </main>
+      <footer className="p-2">
+        <p className="text-center text-gray-500 text-sm">Higor Pires © 2022 - Apegro Challenge </p>
+      </footer>
     </div>
   )
 }
